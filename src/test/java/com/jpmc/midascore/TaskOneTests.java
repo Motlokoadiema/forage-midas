8b461a4e59302fd32f60e83379c1ff5e1ffd8d5d
Mackson <mvckson@outlook.com>
2025-07-23 05:03:52 +0200

    declared dependencies, maintained TaskOneTests code, declared missing test metadata configurations

--- src/test/java/com/jpmc/midascore/TaskOneTests.java
+++ src/test/java/com/jpmc/midascore/TaskOneTests.java
@@ -4,26 +4,32 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+//Import the Awaitability library to handle asynchronous operations if needed
+import org.awaitility.Awaitility;
 
-@SpringBootTest
+@Configuration
+@SpringBootTest(classes = MidasCoreApplication.class)
 class TaskOneTests {
     static final Logger logger = LoggerFactory.getLogger(TaskOneTests.class);
 
-    @Test
-    void task_one_verifier() throws InterruptedException {
-        Thread.sleep(2000);
-        logger.info("----------------------------------------------------------");
-        logger.info("----------------------------------------------------------");
-        logger.info("----------------------------------------------------------");
-        logger.info("Congrats! It looks like your application booted without issue");
-        logger.info("submit the following output to complete the task (include begin and end output denotations)");
-        StringBuilder output = new StringBuilder("\n").append("---begin output ---").append("\n");
-        for (int i = 0; i < 10; i++) {
-            output.append(String.valueOf((int) Math.floor(Math.pow(i, i))));
+// KafkaProducer is now in its own file and managed by Spring
+
+
+        @Test
+        void ckekcdtask_one_verifier() {
+            // Replacing the Thread.sleep(millis:2000) with Awaitility
+            Awaitility.await().atMost(2, java.util.concurrent.TimeUnit.SECONDS).until(() -> true);
+            logger.info("----------------------------------------------------------");
+            logger.info("----------------------------------------------------------");
+            logger.info("----------------------------------------------------------");
+            logger.info("Congrats! It looks like your application booted without issue");
+            logger.info("submit the following output to complete the task (include begin and end output denotations)");
+            StringBuilder output = new StringBuilder("\n").append("---begin output ---").append("\n");
+            for (int i = 0; i < 10; i++) {
+                output.append(String.valueOf((int) Math.floor(Math.pow(i, i))));
+            }
+            output.append("\n").append("---end output ---");
+            logger.info(output.toString());
         }
-        output.append("\n").append("---end output ---");
-        logger.info(output.toString());
-
     }
-
-}
